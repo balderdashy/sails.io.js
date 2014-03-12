9f6d1dff97adb3115164336e1917e7505827be04
Mike McNeil <michael.r.mcneil@gmail.com>
2014-03-12 11:52:23 -0500

    Added Grunt build task and first checkin of build.

---
+++ Gruntfile.js
@@ -0,0 +1,36 @@
+/**
+ * Grunt automation.
+ */
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+
+    PATHS: {
+      socketioClient: './dependencies/socket.io.min.js',
+      sailsio: './sails.io.js',
+      dist: './dist/sails.io.js'
+    },
+
+    concat: {
+      dev: {
+        src: ['<%= PATHS.socketioClient %>', '<%= PATHS.sailsio %>'],
+        dest: '<%= PATHS.dist %>'
+      }
+    },
+
+    watch: {
+      files: ['<%= PATHS.sailsio %>'],
+      tasks: ['concat']
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  grunt.registerTask('default', ['dev']);
+
+  // Dev enviroment for copying over changes from src to example project.
+  grunt.registerTask('dev', ['concat', 'watch']);
+
+
+};
