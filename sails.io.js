3e0b64b753f597fe2354401b6a92120c4d5d7b3f
cxam <me@cxam.co.uk>
2015-04-15 22:13:12 +0000

    fixes by weblogixx which allows the query parameters to be passed to SailsSocket

--- sails.io.js
+++ sails.io.js
@@ -320,6 +320,7 @@
       self.url = opts.url;
       self.multiplex = opts.multiplex;
       self.transports = opts.transports;
+      self.query = opts.query;
 
       // Set up "eventQueue" to hold event handlers which have not been set on the actual raw socket yet.
       self.eventQueue = {};
@@ -354,6 +355,7 @@
       self.useCORSRouteToGetCookie = self.useCORSRouteToGetCookie||io.sails.useCORSRouteToGetCookie;
       self.url = self.url||io.sails.url;
       self.transports = self.transports || io.sails.transports;
+      self.query = self.query || io.sails.query;
 
       // Ensure URL has no trailing slash
       self.url = self.url ? self.url.replace(/(\/)$/, '') : undefined;
