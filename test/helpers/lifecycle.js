885cdadeea241f203d203e4248b444ac2810bd4b
Nick Shvelidze <n.shvelidze@in2circle.com>
2015-12-28 13:12:28 +0400

    Fix done() called multiple times in lifecycle.teardown

--- test/helpers/lifecycle.js
+++ test/helpers/lifecycle.js
@@ -87,10 +87,6 @@ module.exports = {
     
   },
 
-
-
-
-
   teardown: function (done) {
 
     // If the socket never connected, don't worry about disconnecting
@@ -100,23 +96,18 @@ module.exports = {
     if (!global.io || !io.socket || !io.socket.isConnected()) {
       return done();
     }
-    
+
     // Disconnect socket
     io.socket.disconnect();
     setTimeout(function ensureDisconnect () {
       
       // Ensure socket is actually disconnected
       var isActuallyDisconnected = (io.socket.isConnected() === false);
+      delete global.io;
       
       // Tear down sails server
-      global.server.lower(function (){
-
-        // Delete globals (just in case-- shouldn't matter)
-        delete global.server;
-        delete global.io;
-        return done();
-      });
-      
+      global.server.lower();
+      return done();
     }, 0);
   }
 };
