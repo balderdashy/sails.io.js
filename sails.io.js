147952113e9e590ce5b8c1a19f3127fe9efac5c3
Guten Ye <ywzhaifei@gmail.com>
2016-01-13 21:03:58 +0800

    add path options

--- sails.io.js
+++ sails.io.js
@@ -356,6 +356,7 @@
       self.url = self.url||io.sails.url;
       self.transports = self.transports || io.sails.transports;
       self.query = self.query || io.sails.query;
+      self.path = io.sails.path;
 
       // Ensure URL has no trailing slash
       self.url = self.url ? self.url.replace(/(\/)$/, '') : undefined;
