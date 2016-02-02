4ec82c19d3ba434a6499e9d1de4eb244637ee22e
Scott Gress <scottmgress@gmail.com>
2016-02-01 23:02:57 -0600

    Expand queue tests to include tests for request queue on disconnect/reconnect

--- test/queue.test.js
+++ test/queue.test.js
@@ -12,59 +12,146 @@ var async = require('async');
 // forget to shut down another Sails app
 var TEST_SERVER_PORT = 1577;
 
+describe('queueing :: ', function() {
 
-describe('before connecting, socket', function () {
+  describe('requests :: ', function() {
 
+    var socket;
+    var app;
+    var fnContainer = {};
 
-  it('should allow requests to be queued, then execute their callbacks when the socket connects and is able to process the queue', function (done){
-    
-    _buildSailsApp(function (err, app){
-      if (err) return done(err);
+    before(function() {
 
-      app.get('/foo', function (req,res){
-        res.send('foo');
-      });
-      
-      var io = _getFreshClient();
+      var socketIOClient = require('socket.io-client');
+      // Instantiate a sails.io.js client and configure the url.
+      io = sailsIO(socketIOClient);
+      io.sails.url = 'http://localhost:'+TEST_SERVER_PORT;
+      io.sails.autoConnect = false;
+      // Disable logger in sails.io.js client
+      io.sails.environment = 'production';
+      socket = io.sails.connect();
+      socket.on('foo', function(){console.log("FFOOOOO!");});
+    });
 
-      //
-      // Notice we're not waiting for the connection to do this.
-      //
+    after(function(done) {
+      app.lower(done);
+    });
 
-      io.socket.get('/foo', function (body, jwr) {
-        assert(body==='foo');
-        assert(jwr.statusCode===200);
-
-        app.lower(function () {
-          return done();
+    describe('before connecting, socket', function () {
+      it('should allow requests to be queued', function() {
+        socket.get('/foo', function (body, jwr) {
+          fnContainer.fn(body, jwr);
         });
       });
+
+    });
+
+    describe('after connecting, socket', function () {
+
+      it('should execute queued requests successfully', function(done) {
+        fnContainer.fn = function(body, jwr) {
+          assert(body==='foo');
+          assert(jwr.statusCode===200);
+          return done();
+        };
+        _buildSailsApp(function (err, _app){
+          if (err) {return done(err);}
+          app = _app;
+          app.get('/foo', function (req,res){
+            res.send('foo');
+          });
+        });
+      });
+
+    });
+
+    describe('after being disconnected, socket', function() {
+      before(function(done) {
+        app.lower(function(err) {
+          if (err) {return done(err);}
+          setTimeout(done, 100);
+        });
+      });
+      it('should allow requests to be queued', function() {
+        socket.get('/bar', function (body, jwr) {
+          fnContainer.fn(body, jwr);
+        });
+      });
+    });
+
+    describe('after reconnecting, socket', function () {
+
+      it('should execute queued requests successfully', function(done) {
+        fnContainer.fn = function(body, jwr) {
+          assert(body==='bar');
+          assert(jwr.statusCode===200);
+          return done();
+        };
+        _buildSailsApp(function (err, _app){
+          if (err) {return done(err);}
+          app = _app;
+          app.get('/bar', function (req,res){
+            res.send('bar');
+          });
+        });
+      });
+
+    });
+
+  });
+
+  describe('should allow requests to be queued', function() {
+
+    it('then execute their callbacks when the socket connects and is able to process the queue', function (done){
+
+      _buildSailsApp(function (err, app){
+        if (err) return done(err);
+
+        app.get('/foo', function (req,res){
+          res.send('foo');
+        });
+
+        var io = _getFreshClient();
+
+        //
+        // Notice we're not waiting for the connection to do this.
+        //
+
+        io.socket.get('/foo', function (body, jwr) {
+          assert(body==='foo');
+          assert(jwr.statusCode===200);
+
+          app.lower(function () {
+            return done();
+          });
+        });
+      });
+
     });
 
   });
 
 
   it('should allow comet listeners to be bound to the fake TmpSocket, then rebind them to the actual socket once it connects and is therefore capable of receiving events', function (done){
-    
+
     // Note that we're not actually QUEUING comet events that were sent
     // while the socket connects-- there's no way to do that client-side,
     // and it wouldn't really be that helpful.
-    // 
+    //
     // Instead, the purpose of this test is to make sure that the listeners
     // "make it" to the real socket once it does connect.
 
     _buildSailsApp(function (err, app){
       if (err) return done(err);
-      
       var io = _getFreshClient();
-
+      tio = io;
       //
       // Notice we're not waiting for the connection to do this.
       //
 
       io.socket.on('foo', function (event){
         assert('oh hi' === event);
-        app.lower(function () {
+        app.lower({hardShutdown: true}, function () {
           return done();
         });
       });
@@ -81,13 +168,12 @@ describe('before connecting, socket', function () {
 
 
 
- it('should allow MULTIPLE comet listeners to be bound to the SAME EVENT before the socket has connected, then rebind them to the actual socket once it connects', function (done){
-    
+  it('should allow MULTIPLE comet listeners to be bound to the SAME EVENT before the socket has connected, then rebind them to the actual socket once it connects', function (done){
+
     _buildSailsApp(function (err, app){
       if (err) return done(err);
-      
-      var io = _getFreshClient();
 
+      var io = _getFreshClient();
       //
       // Notice we're not waiting for the connection to do this.
       //
@@ -127,8 +213,6 @@ describe('before connecting, socket', function () {
 });
 
 
-
-
 /**
  * @optional  {String} url
  * @return {io}
@@ -147,10 +231,11 @@ function _getFreshClient (url) {
 
   // Instantiate a sails.io.js client and configure the url.
   var io = sailsIO(socketIOClient);
+  io.sails.reconnection = false;
   io.sails.url = url||'http://localhost:'+TEST_SERVER_PORT;
   // Disable logger in sails.io.js client
   io.sails.environment = 'production';
-  
+
   return io;
 }
 
@@ -162,7 +247,7 @@ function _buildSailsApp(cb) {
   var app = new Sails();
   app.lift({
     port: TEST_SERVER_PORT,
-    log: {level:'silent'},
+    log: {level:'error'},
     hooks: {grunt: false}
   },function (err) {
     if (err) return cb(err);
