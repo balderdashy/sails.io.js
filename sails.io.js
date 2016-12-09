300b18ce9be90e375e8d563f8c3ef4adcb890d04
Scott Gress <scottmgress@gmail.com>
2016-12-09 09:58:49 -0600

    1.1.4

--- sails.io.js
+++ sails.io.js
@@ -118,7 +118,7 @@
    * @type {Dictionary}
    */
   var SDK_INFO = {
-    version: '1.1.3', // <-- pulled automatically from package.json, do not change!
+    version: '1.1.4', // <-- pulled automatically from package.json, do not change!
     language: 'javascript',
     platform: (function (){
       if (typeof module === 'object' && typeof module.exports !== 'undefined') {
