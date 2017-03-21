balderdashy/sails.io.js

commit bb174df43205b415755ef60071c811f1094fe3fb

Set up eslint ignore for distributable.

Gruntfile.js
@@ -18,6 +18,8 @@ module.exports = function(grunt) {
   grunt.initConfig({
 
     PATHS: {
+      eslintEnable: './dependencies/eslint-enable.txt',
+      eslintDisable: './dependencies/eslint-disable.txt',
       amdInstructions: './dependencies/amd_instructions.js',
       socketioClient: './dependencies/socket.io.min.js',
       header: './.tmp/header.txt',
@@ -66,14 +68,23 @@ module.exports = function(grunt) {
 
     concat: {
       header: {
-        src: ['<%= PATHS.amdInstructions %>', '<%= PATHS.socketioClientVersion %>', '<%= PATHS.socketioClient %>'],
+        src: [
+          '<%= PATHS.eslintEnable %>',
+          '<%= PATHS.amdInstructions %>',
+          '<%= PATHS.socketioClientVersion %>',
+          '<%= PATHS.socketioClient %>',
+        ],
         dest: '<%= PATHS.header %>'
       },
       main: {
         options: {
           separator: ';\n\n',
         },
-        src: ['<%= PATHS.header %>', '<%= PATHS.sailsio %>'],
+        src: [
+          '<%= PATHS.header %>',
+          '<%= PATHS.sailsio %>',
+          '<%= PATHS.eslintDisable %>',
+        ],
         dest: '<%= PATHS.dist %>'
       }
     },

dist/sails.io.js
@@ -1,3 +1,5 @@
+/* eslint-enable */
+
 /**
  * To use sails.io.js in an AMD environment (e.g. with require.js),
  * replace this file with the sails.io.js file from the root of:
@@ -1605,3 +1607,6 @@ this.area.value=a.replace(i,"\\n");try{this.form.submit()}catch(l){}this.iframe.
   }
 
 })();
+;
+
+/* eslint-disable */

commit 216be51074cbe30a7258ee054dd60339c46eeba3

Put the enable and the disable in the right place. (fixes issue introduced by previous commit: bb174df43205b415755ef60071c811f1094fe3fb)

Gruntfile.js
@@ -69,7 +69,7 @@ module.exports = function(grunt) {
     concat: {
       header: {
         src: [
-          '<%= PATHS.eslintEnable %>',
+          '<%= PATHS.eslintDisable %>',
           '<%= PATHS.amdInstructions %>',
           '<%= PATHS.socketioClientVersion %>',
           '<%= PATHS.socketioClient %>',
@@ -83,7 +83,7 @@ module.exports = function(grunt) {
         src: [
           '<%= PATHS.header %>',
           '<%= PATHS.sailsio %>',
-          '<%= PATHS.eslintDisable %>',
+          '<%= PATHS.eslintEnable %>',
         ],
         dest: '<%= PATHS.dist %>'
       }

dist/sails.io.js
@@ -1,4 +1,4 @@
-/* eslint-enable */
+/* eslint-disable */
 
 /**
  * To use sails.io.js in an AMD environment (e.g. with require.js),
@@ -1609,4 +1609,4 @@ this.area.value=a.replace(i,"\\n");try{this.form.submit()}catch(l){}this.iframe.
 })();
 ;
 
-/* eslint-disable */
+/* eslint-enable */